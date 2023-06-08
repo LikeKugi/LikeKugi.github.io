fd109a6cb86ca6dc409b61d5f664824ecb9596d5
Ilya Shcherbakov <86749581+LikeKugi@users.noreply.github.com>
2023-06-09 01:12:28 +0300

    fix: change link in works

--- euclid/js/Works.js
+++ euclid/js/Works.js
@@ -1,18 +1,18 @@
 class Works {
   #articles;
-  constructor(elem = ".working") {
+  constructor(elem = '.working') {
     this.section = document.querySelector(elem);
-    this.controls = [...this.section.querySelectorAll(".tabs__btn")];
-    this.title = this.section.querySelector(".working__subtitle");
-    this.text = this.section.querySelector(".working__text");
-    this.image = this.section.querySelector(".working__img");
+    this.controls = [...this.section.querySelectorAll('.tabs__btn')];
+    this.title = this.section.querySelector('.working__subtitle');
+    this.text = this.section.querySelector('.working__text');
+    this.image = this.section.querySelector('.working__img');
     this.#articles = [];
     this.fetchData();
     this.createListeners();
   }
 
   async fetchData() {
-    let response = await fetch("/js/works.json");
+    let response = await fetch('./js/works.json');
     try {
       if (!response.ok) return false;
       let ans = await response.json();
@@ -26,21 +26,21 @@ class Works {
     console.log(target);
     this.controls.forEach((el, i) => {
       console.log(el);
-      if (i === target) el.classList.add("tabs__btn--active");
-      else el.classList.remove("tabs__btn--active");
+      if (i === target) el.classList.add('tabs__btn--active');
+      else el.classList.remove('tabs__btn--active');
     });
   }
 
   renderData(index) {
     this.markActive(index);
-    this.title.textContent = this.#articles[index]["title"];
-    this.text.textContent = this.#articles[index]["text"];
-    this.image.src = this.#articles[index]["image"];
+    this.title.textContent = this.#articles[index]['title'];
+    this.text.textContent = this.#articles[index]['text'];
+    this.image.src = this.#articles[index]['image'];
   }
 
   createListeners() {
     this.controls.forEach((el, i) => {
-      el.addEventListener("click", () => {
+      el.addEventListener('click', () => {
         this.renderData(i);
       });
     });
